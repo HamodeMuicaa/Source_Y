89e3846ead2b72f2f9fe4d8ebb9838fb5a5d7bb6
⛥ 𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳 . <167021222+yousef1yt@users.noreply.github.com>
2024-10-06 17:18:15 +0300

    Uploaded Successfully

---
+++ YousefMusic/core/dir.py
@@ -0,0 +1,20 @@
+import os
+
+from ..logging import LOGGER
+
+# 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 ⛥ 𓏺 Yousef .tele_https://t.me/y_o_v
+def dirr():
+    for file in os.listdir():
+        if file.endswith(".jpg"):
+            os.remove(file)
+        elif file.endswith(".jpeg"):
+            os.remove(file)
+        elif file.endswith(".png"):
+            os.remove(file)
+
+    if "downloads" not in os.listdir():
+        os.mkdir("downloads")
+    if "cache" not in os.listdir():
+        os.mkdir("cache")
+
+    LOGGER("ميــوزك بحر،").info("تم تحـديث السـورس ...✓")
