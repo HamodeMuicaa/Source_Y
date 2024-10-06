64f39a637edf14c07c5a57b34ad96263b0d0d192
⛥ 𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳 . <167021222+yousef1yt@users.noreply.github.com>
2024-10-06 17:20:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -48,9 +48,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/yousef1yt/Make",
+    "https://github.com/yousef1yt/Source_Y",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Yousef")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
